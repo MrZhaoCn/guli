7432e28ea86121a28a2236bb5765754588dec99e
zhaocun682 <zhaocun682@hellobike.com>
2022-03-10 19:58:00 +0800

    first commit

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,35 @@
+package com.atguigu.eduservice.controller;
+
+
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-03-10
+ */
+@RestController
+@RequestMapping("/eduservice/eduteacher")
+public class EduTeacherController {
+    @Autowired
+    private EduTeacherService teacherService;
+
+    @GetMapping
+    public List<EduTeacher> getAllTeacher() {
+        List <EduTeacher> list = teacherService.list(null);
+        return list;
+    }
+
+}
+
